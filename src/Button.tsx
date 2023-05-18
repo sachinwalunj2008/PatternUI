e663107c8a86e885013773cdf388a31add6e64d7
sachin-walunj <sachin.walunj@pattern.com>
2023-05-18 16:08:02 +0530

    ewew

--- src/Button.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './Button.scss'
-
-export default function ({ title }: {title: string}) {
-    return (
-        <button className="UI-Button">{title}</button>
-    )
-}
